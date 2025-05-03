71e4a5b37c0638f185408e850e3ad88e05b20300
Hinajawaid <j.hinaxtick@gmail.com>
2025-05-03 12:56:21 +0500

    resources backend

--- Index.js
+++ Index.js
@@ -8,13 +8,13 @@ import spellingGameRouter from "./modules/spellingGame/index.js";
 import phonicsRoutes from "./modules/phonicsGame/index.js";
 import audioRoutes from "./modules/audio/index.js";
 import adminRouter from "./modules/admin/index.js";
-import contentRouter from './modules/content/index.js';
+import contentRouter from "./modules/content/index.js";
 import resourcesRouter from "./modules/resources/index.js";
 import gameRouter from "./modules/game/index.js";
 import axios from "axios";
 import bodyParser from "body-parser";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 import textToSpeechRouter from "./modules/textToSpeech/index.js";
 
@@ -31,18 +31,18 @@ app.use(
 );
 
 const __filename = fileURLToPath(import.meta.url);
-app.use(express.static(path.join(__dirname, 'public'))); // Add this line
-
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public"))); // Add this line
 
 app.use(bodyParser.json());
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 app.use("/api/admin", adminRouter);
 app.use("/user", userRouter);
 app.use("/planner", todoRouter);
 app.use("/spelling", spellingGameRouter);
 app.use("/phonics", phonicsRoutes);
 app.use("/audio", audioRoutes);
-app.use('/api/content', contentRouter);
+app.use("/api/content", contentRouter);
 
 app.use("/game", gameRouter);
 console.log("Audio routes mounted at /audio");
